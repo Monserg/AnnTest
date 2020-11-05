61f05117591c1eb8c9d93047a0f782cdec91ca85
Sergey Monasturskiy <msm72@mail.ru>
2020-11-05 20:50:27 +0200

    Add Task file.

---
+++ AnnTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnnTest
+//
+//  Created by Sergey Monastyrskiy on 05.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
